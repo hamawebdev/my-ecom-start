449ddff11cb55ec9dd104e9c5ad175e613de7bd8
hamawebdev <148373753+hamawebdev@users.noreply.github.com>
2025-03-07 22:58:12 +0100

    restore navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,10 +57,10 @@ interface Navbar1Props {
 
 const Navbar1 = ({
   logo = {
-    url: "/",
+    url: "https://www.shadcnblocks.com",
     src: "https://www.shadcnblocks.com/images/block/block-1.svg",
     alt: "logo",
-    title: "Acme",
+    title: "Shadcnblocks.com",
   },
   menu = [
     { title: "Home", url: "#" },
@@ -135,18 +135,18 @@ const Navbar1 = ({
     },
   ],
   auth = {
-    login: { text: "Log in", url: "login" },
-    signup: { text: "Sign up", url: "signup" },
+    login: { text: "Log in", url: "#" },
+    signup: { text: "Sign up", url: "#" },
   },
 }: Navbar1Props) => {
   return (
-    <section className="py-4 border-b border-border">
+    <section className="py-4">
       <div className="container">
         {/* Desktop Menu */}
         <nav className="hidden justify-between lg:flex">
           <div className="flex items-center gap-6">
             <a href={logo.url} className="flex items-center gap-2">
-              <img src={logo.src} className="w-8 dark:invert" alt={logo.alt} />
+              <img src={logo.src} className="w-8" alt={logo.alt} />
               <span className="text-lg font-semibold">{logo.title}</span>
             </a>
             <div className="flex items-center">
@@ -170,7 +170,7 @@ const Navbar1 = ({
         <div className="block lg:hidden">
           <div className="flex items-center justify-between">
             <a href={logo.url} className="flex items-center gap-2">
-              <img src={logo.src} className="w-8 dark:invert" alt={logo.alt} />
+              <img src={logo.src} className="w-8" alt={logo.alt} />
               <span className="text-lg font-semibold">{logo.title}</span>
             </a>
             <Sheet>
@@ -183,7 +183,7 @@ const Navbar1 = ({
                 <SheetHeader>
                   <SheetTitle>
                     <a href={logo.url} className="flex items-center gap-2">
-                      <img src={logo.src} className="w-8 dark:invert" alt={logo.alt} />
+                      <img src={logo.src} className="w-8" alt={logo.alt} />
                       <span className="text-lg font-semibold">
                         {logo.title}
                       </span>
@@ -220,7 +220,7 @@ const Navbar1 = ({
 const renderMenuItem = (item: MenuItem) => {
   if (item.items) {
     return (
-      <NavigationMenuItem key={item.title}>
+      <NavigationMenuItem key={item.title} className="text-muted-foreground">
         <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
         <NavigationMenuContent>
           {item.items.map((subItem) => (
@@ -236,7 +236,7 @@ const renderMenuItem = (item: MenuItem) => {
   return (
     <a
       key={item.title}
-      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground"
+      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
       href={item.url}
     >
       {item.title}
@@ -270,14 +270,14 @@ const renderMobileMenuItem = (item: MenuItem) => {
 const SubMenuLink = ({ item }: { item: MenuItem }) => {
   return (
     <a
-      className="flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+      className="flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-muted hover:text-accent-foreground"
       href={item.url}
     >
       <div>{item.icon}</div>
       <div>
-        <div className="text-sm font-semibold leading-none">{item.title}</div>
+        <div className="text-sm font-semibold">{item.title}</div>
         {item.description && (
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <p className="text-sm leading-snug text-muted-foreground">
             {item.description}
           </p>
         )}
