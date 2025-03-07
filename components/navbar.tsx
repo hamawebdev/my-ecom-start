7cec4b50a9060eca94a40690b121de3869434fca
hamawebdev <148373753+hamawebdev@users.noreply.github.com>
2025-03-07 21:18:18 +0100

    added small border in the bottom of the header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -140,7 +140,7 @@ const Navbar1 = ({
   },
 }: Navbar1Props) => {
   return (
-    <section className="py-4">
+    <section className="py-4 border-b border-border">
       <div className="container">
         {/* Desktop Menu */}
         <nav className="hidden justify-between lg:flex">
